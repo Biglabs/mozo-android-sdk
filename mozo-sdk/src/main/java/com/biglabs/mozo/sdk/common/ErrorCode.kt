bcda235615aa7f2d40149c41dfbbd9d4be13779f
Vu Quang Nguyen <vu.nguyen.quang@big-labs.com>
2019-01-24 16:21:17 +0700

    Update Error code for wallet different case

--- mozo-sdk/src/main/java/com/biglabs/mozo/sdk/common/ErrorCode.kt
+++ mozo-sdk/src/main/java/com/biglabs/mozo/sdk/common/ErrorCode.kt
@@ -7,5 +7,7 @@ enum class ErrorCode(val key: String, @StringRes val message: Int) {
     ERROR_FATAL("INTERNAL_ERROR", R.string.error_fatal),
     ERROR_REQUIRED_LOGIN("INVALID_USER_TOKEN", R.string.error_required_login),
 
-    ERROR_WALLET_DIFFERENT("SOLOMON_USER_PROFILE_WALLET_INVALID_UPDATE_EXISTING_WALLET_ADDRESS", R.string.error_wallet_different)
+    ERROR_WALLET_ADDRESS_IN_USED("SOLOMON_USER_PROFILE_WALLET_ADDRESS_IN_USED", R.string.error_wallet_different),
+    ERROR_WALLET_ADDRESS_EXISTING("SOLOMON_USER_PROFILE_WALLET_INVALID_UPDATE_EXISTING_WALLET_ADDRESS", R.string.error_wallet_different),
+    ERROR_WALLET_DIFFERENT("SOLOMON_FATAL_USE_DIFFERENT_OFFCHAIN_ADDRESS", R.string.error_wallet_different)
 }
