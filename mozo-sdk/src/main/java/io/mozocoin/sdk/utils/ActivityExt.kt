14dd1d511dd597ae884c960c3fc7f022d03c056d
Vu Quang Nguyen <vu.nguyen.quang@big-labs.com>
2019-07-12 10:22:51 +0700

    Improve ErrorDialog

---
+++ mozo-sdk/src/main/java/io/mozocoin/sdk/utils/ActivityExt.kt
@@ -0,0 +1,43 @@
+package io.mozocoin.sdk.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.ViewGroup
+
+fun Activity.setMatchParent() {
+    val attrs = window.attributes
+    attrs.width = ViewGroup.LayoutParams.MATCH_PARENT
+    attrs.height = ViewGroup.LayoutParams.MATCH_PARENT
+    window.attributes = attrs
+}
+
+inline fun <reified T : Any> Activity.launchActivity(
+        requestCode: Int = -1,
+        options: Bundle? = null,
+        noinline init: Intent.() -> Unit = {}) {
+    val intent = newIntent<T>(this)
+    intent.init()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        startActivityForResult(intent, requestCode, options)
+    } else {
+        startActivityForResult(intent, requestCode)
+    }
+}
+
+inline fun <reified T : Any> Context.launchActivity(
+        options: Bundle? = null,
+        noinline init: Intent.() -> Unit = {}) {
+    val intent = newIntent<T>(this)
+    intent.init()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        startActivity(intent, options)
+    } else {
+        startActivity(intent)
+    }
+}
+
+inline fun <reified T : Any> newIntent(context: Context): Intent =
+        Intent(context, T::class.java)
