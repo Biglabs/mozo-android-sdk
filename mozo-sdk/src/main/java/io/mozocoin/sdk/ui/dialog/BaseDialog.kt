69d66ca539fd3d03e73b330672f129ba6b51a109
Vu Nguyen <vu.nguyen.quang@big-labs.com>
2019-10-11 17:22:34 +0700

    Activity safe check when show dialog

--- mozo-sdk/src/main/java/io/mozocoin/sdk/ui/dialog/BaseDialog.kt
+++ mozo-sdk/src/main/java/io/mozocoin/sdk/ui/dialog/BaseDialog.kt
@@ -1,5 +1,6 @@
 package io.mozocoin.sdk.ui.dialog
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
@@ -9,7 +10,7 @@ import android.view.Window
 import android.view.WindowManager
 import io.mozocoin.sdk.R
 
-open class BaseDialog(context: Context) : Dialog(context) {
+open class BaseDialog(val ctx: Context) : Dialog(ctx) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +25,11 @@ open class BaseDialog(context: Context) : Dialog(context) {
             setGravity(Gravity.CENTER)
             setDimAmount(value.float)
         }
+    }
+
+    override fun show() {
+        if (ctx is Activity && (ctx.isFinishing || ctx.isDestroyed)) return
+
+        super.show()
     }
 }
