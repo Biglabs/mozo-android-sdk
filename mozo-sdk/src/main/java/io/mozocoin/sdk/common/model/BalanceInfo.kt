23b85bd3800c887c5e9b4800d8c1422337d8b322
Vu Nguyen <vu.nguyen.quang@big-labs.com>
2019-10-22 11:46:31 +0700

    Prevent crash when balance null

--- mozo-sdk/src/main/java/io/mozocoin/sdk/common/model/BalanceInfo.kt
+++ mozo-sdk/src/main/java/io/mozocoin/sdk/common/model/BalanceInfo.kt
@@ -1,16 +1,21 @@
 package io.mozocoin.sdk.common.model
 
 import android.os.Parcelable
+import io.mozocoin.sdk.common.Constant
 import io.mozocoin.sdk.utils.Support
+import io.mozocoin.sdk.utils.safe
 import kotlinx.android.parcel.Parcelize
 import java.math.BigDecimal
 
 @Parcelize
 data class BalanceInfo(
-        val balance: BigDecimal,
+        val balance: BigDecimal?,
         val symbol: String?,
-        val decimals: Int,
+        val decimals: Int?,
         val contractAddress: String?
 ) : Parcelable {
-    fun balanceNonDecimal(): BigDecimal = Support.toAmountNonDecimal(balance, decimals)
+    fun balanceNonDecimal(): BigDecimal = Support.toAmountNonDecimal(
+            balance.safe(),
+            decimals ?: Constant.DEFAULT_DECIMAL
+    )
 }
