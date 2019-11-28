357e6a13acf5c826f28c06605f0785d421edb76d
chuongpro184 <chuong.nt@big-labs.com>
2019-11-28 10:17:09 +0700

    Update DTO BroadcastData

--- mozo-sdk/src/main/java/io/mozocoin/sdk/common/model/BroadcastData.kt
+++ mozo-sdk/src/main/java/io/mozocoin/sdk/common/model/BroadcastData.kt
@@ -3,14 +3,12 @@ package io.mozocoin.sdk.common.model
 import com.google.gson.Gson
 
 data class BroadcastData(
-        val time: Long,
-        val content: String,
-        val imageId: String
+    val time: Long,
+    val content: String
 ) {
     fun getData() = try {
         Gson().fromJson(content, BroadcastDataContent::class.java)?.apply {
             time = this@BroadcastData.time
-            imageId = this@BroadcastData.imageId
         }
     } catch (e: Exception) {
         null
