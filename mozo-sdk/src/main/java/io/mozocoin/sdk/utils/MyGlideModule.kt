c90f20b18e1e2f9a90598e0c15d24f73cae13d9e
vuquangnguyen-biglabs <vu.nguyen.quang@big-labs.com>
2021-02-24 14:32:07 +0700

    Improve custom Glide module

--- mozo-sdk/src/main/java/io/mozocoin/sdk/utils/MyGlideModule.kt
+++
@@ -1,7 +0,0 @@
-package io.mozocoin.sdk.utils
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.LibraryGlideModule
-
-@GlideModule
-class MyGlideModule : LibraryGlideModule()
