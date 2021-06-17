2794b3f78d7264dd8832b689db51d8378024357f
Vu Nguyen <vu.nguyen.quang@big-labs.com>
2021-06-17 13:37:59 +0700

    expose callback to open details when tap on contact info from Tx history

--- mozo-sdk/src/main/java/io/mozocoin/sdk/common/model/Contact.kt
+++ mozo-sdk/src/main/java/io/mozocoin/sdk/common/model/Contact.kt
@@ -1,47 +1,21 @@
 package io.mozocoin.sdk.common.model
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Contact(
-        @Transient
-        val id: Long,
-        @SerializedName(value = "name", alternate = ["storeName"]) val name: String? = null,
-        @SerializedName("storePhysicalAddress") val physicalAddress: String? = null,
-        @SerializedName(value = "soloAddress", alternate = ["storeOffchainAddress"]) val soloAddress: String? = null,
-        val phoneNo: String? = null,
-        @Transient
-        var isStore: Boolean = false
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readLong(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readByte() != 0.toByte())
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeLong(id)
-        parcel.writeString(name)
-        parcel.writeString(physicalAddress)
-        parcel.writeString(soloAddress)
-        parcel.writeString(phoneNo)
-        parcel.writeByte(if (isStore) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Contact> {
-        override fun createFromParcel(parcel: Parcel): Contact {
-            return Contact(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Contact?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    @Transient
+    val id: Long,
+    @SerializedName(value = "name", alternate = ["storeName"]) val name: String? = null,
+    @SerializedName("storePhysicalAddress") val physicalAddress: String? = null,
+    @SerializedName(
+        value = "soloAddress",
+        alternate = ["storeOffchainAddress"]
+    ) val soloAddress: String? = null,
+    val phoneNo: String? = null,
+    val storeId: Long? = null,
+    @Transient
+    var isStore: Boolean = false
+) : Parcelable
